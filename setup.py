d942d6705e8338bdcfd750d43a02893ca95cbd4a
PauBadiaM <paubadiamompel@gmail.com>
2022-02-08 10:10:44 +0100

    Added requirements

--- setup.py
+++ setup.py
@@ -4,6 +4,13 @@ setup(
     name="foo",
     version="0.0.1",
     packages=['foo'],
+    install_requires=["numba",
+                      "sklearn",
+                      "tqdm",
+                      "anndata",
+                      "skranger",
+                      "fisher"
+                     ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
