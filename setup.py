225f64ace12b8ee620aea9b2e70b176fbb76dd2f
PauBadiaM <paubadiamompel@gmail.com>
2022-02-08 11:42:04 +0100

    numpy to 1.20.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
                       "anndata",
                       "skranger",
                       "fisher==0.1.9",
-                      "numpy==1.21.0"
+                      "numpy==1.20.0"
                      ],
     classifiers=[
         "Programming Language :: Python :: 3",
